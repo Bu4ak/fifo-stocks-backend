1cd698adb9cb5b950cc5c86d63eca01f183b3476
bu4ak <igorbu4@gmail.com>
2020-07-02 22:44:13 +0200

    Fix cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,22 +15,15 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
-        ];
-
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
         $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
+
+        $response->header("Access-Control-Allow-Origin", env('FRONT_DOMAIN', '*'));
+        $response->header("Access-Control-Allow-Credentials", "true");
+        $response->header("Access-Control-Max-Age", "600");
+
+        $response->header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+
+        $response->header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With, Application");
 
         return $response;
     }
